ca2273c79d9baa6a067a96cbaa280eb2de091464
goorilas <wdxz_100@hotmail.com>
2024-06-29 11:45:52 -0300

    exercicio ajax

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameE = document.querySelector('#name');
+    const usernameE = document.querySelector('#username');
+    const avatarE = document.querySelector('#avatar');
+    const reopsE = document.querySelector('#repos');
+    const followingE = document.querySelector('#following');
+    const followersE = document.querySelector('#followers');
+    const linkE = document.querySelector('#link');
+    
+    fetch('https://api.github.com/users/goorilas')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        nameE.innerText = json.name;
+        usernameE.innerText = json.login;
+        avatarE.src = json.avatar_url;
+        followingE.innerText = json.following;
+        followersE.innerText = json.followers;
+        reopsE.innerText = json.public_repos;
+        linkE.href = json.html_url;
+    })
+})
